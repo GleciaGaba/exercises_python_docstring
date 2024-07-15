8e16b89d725e3e5062b58be96a1f684480f638fb
GleciaGaba <gleciasf@hotmail.com>
2024-07-15 14:33:11 +0200

    first commit

---
+++ docstring.py
@@ -0,0 +1,50 @@
+"""
+
+Les docstrings (ou chaînes de documentation) sont des chaînes littérales qui apparaissent
+juste après la définition d'une fonction, d'une méthode, d'une classe ou d'un module,
+et qui sont utilisées pour documenter l'objet. Elles sont essentielles pour écrire du code
+Python lisible et bien documenté. Voici quelques points clés sur les docstrings :
+
+"""
+
+
+"""
+Les formats de documentation Epytext, reStructuredText (reST) et Google sont utilisés pour 
+structurer les docstrings dans les projets Python. Chacun a ses propres conventions et syntaxe 
+pour la documentation. Voici une définition et une comparaison de chacun :
+"""
+
+#  Epytext
+"""
+Docstring de style Epytext
+
+@param param1: un premier paramètre
+@param param2: un autre paramètre
+@return: description de ce qui est retourné
+
+"""
+
+
+#  reST
+"""
+Docstring de style reST
+
+:param param1: un premier paramètre
+:param param2: un autre paramètre
+:return: description de ce qui est retourné
+
+"""
+
+#  Google
+
+"""
+Docstring de style Google
+
+Args:
+  param1: un premier paramètre
+  param2: un autre paramètre
+  
+Return: 
+  Description de ce qui est retourné
+
+"""
---
+++ docstring_test.py
@@ -0,0 +1,23 @@
+def fonction_example(nom, age):
+    """Description de la fonction   
+
+    Args:
+        nom (str): Le nom d'utilisateur
+        age (int): L'age d'utilisateur
+    Returns:
+        list: Liste de nombres
+    
+    """
+    return [1, 2, 3]
+
+
+def fonction_example1(nom, age):
+    """
+
+    Args:
+        nom:
+        age:
+
+    Returns:
+
+    """
